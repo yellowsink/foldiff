ba4c9f392463faac8e9c77178f2cc18332f3f3f1
Hazel Atkinson <yellowsink@riseup.net>
2024-09-13 23:45:32 +0100

    switch to join, remove unnecessary Mismatch type

--- src/verify.rs
+++ src/verify.rs
@@ -4,33 +4,9 @@ use anyhow::{bail, Result};
 use indicatif::ProgressBar;
 use rayon::prelude::*;
 use std::collections::BTreeSet;
-use std::fmt::{Display, Formatter};
+use std::fmt::Display;
 use std::fs;
-use std::path::{Path, PathBuf};
-
-pub enum Mismatch {
-	TypeMismatch((PathBuf, PathBuf)), // (File, Folder)
-	HashMismatch(PathBuf),
-	OnlyIn((PathBuf, bool)), // bool is "is in second dir"
-}
-
-impl Display for Mismatch {
-	fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-		match self {
-			Mismatch::TypeMismatch((p1, p2)) => 
-				write!(f, "{p1:?} is a file, but {p2:?} is a folder, thus they mismatch."),
-			Mismatch::HashMismatch(p) =>
-				write!(f, "The file {p:?} exists in both directories, but has differing contents."),
-			Mismatch::OnlyIn((p, in_second)) =>
-				if *in_second {
-					write!(f, "{p:?} only exists in the second folder.")
-				}
-				else {
-					write!(f, "{p:?} only exists in the first folder.")
-				},
-		}
-	}
-}
+use std::path::Path;
 
 /// Checks if two directories are identical, printing results to stdout
 pub fn test_equality(r1: &Path, r2: &Path) -> Result<()> {
@@ -60,28 +36,26 @@ fn test_equality_internal(r1: &Path, r2: &Path, p: &Path, spn: &ProgressBar) -> 
 		if type2.is_file() {
 			if hash_file(&path1)? != hash_file(&path2)? {
 				spn.suspend(|| {
-					println!("{}", Mismatch::HashMismatch(p.to_path_buf()));
+					println!("The file {:?} exists in both directories, but has differing contents.", p.to_path_buf());
 				});
 			}
 		}
 		else {
 			spn.suspend(|| {
-				println!("{}",
-							Mismatch::TypeMismatch((
-								Path::new(r1.file_name().unwrap()).join(p),
-								Path::new(r2.file_name().unwrap()).join(p)
-							))
+				println!(
+					"{:?} is a file, but {:?} is a folder, thus they mismatch.",
+					Path::new(r1.file_name().unwrap()).join(p),
+					Path::new(r2.file_name().unwrap()).join(p)
 				);
 			});
 		}
 	}
 	else if type2.is_file() {
 		spn.suspend(|| {
-			println!("{}",
-						Mismatch::TypeMismatch((
-							Path::new(r2.file_name().unwrap()).join(p),
-							Path::new(r1.file_name().unwrap()).join(p)
-						))
+			println!(
+				"{:?} is a folder, but {:?} is a file, thus they mismatch.",
+				Path::new(r1.file_name().unwrap()).join(p),
+				Path::new(r2.file_name().unwrap()).join(p)
 			);
 		});
 	}
@@ -96,15 +70,15 @@ fn test_equality_internal(r1: &Path, r2: &Path, p: &Path, spn: &ProgressBar) -> 
 		
 		let mut rec_res = anyhow::Ok(());
 		// do the loops in parallel
-		rayon::scope(|s| {
+		rayon::join(
 			// check for files only in set 1, and for files in both
-			s.spawn(|_| {
+			|| {
 				rec_res =
 					set1.par_iter()
 						.map(|f| {
 							if !set2.contains(f) {
 								spn.suspend(|| {
-									println!("{}", Mismatch::OnlyIn((p.join(f), false)));
+									println!("{:?} only exists in the first folder.", p.join(f));
 								});
 								spn.inc(1);
 							}
@@ -115,20 +89,18 @@ fn test_equality_internal(r1: &Path, r2: &Path, p: &Path, spn: &ProgressBar) -> 
 							Ok(())
 						})
 						.collect();
-			});
-			// check for files only in set 2
-			s.spawn(|_| {
+			},
+			||
 				set2.par_iter()
 					.for_each(|f| {
 						if !set1.contains(f) {
 							spn.suspend(|| {
-								println!("{}", Mismatch::OnlyIn((p.join(f), true)));
+								println!("{:?} only exists in the second folder.", p.join(f));
 							});
 							spn.inc(1);
 						}
-					});
-			});
-		});
+					}),
+		);
 		
 		rec_res?;
 	}
